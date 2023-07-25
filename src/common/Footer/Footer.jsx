1102b7a0d6a49e2308da9e65e85f2261ee93a7cf
DavidTL95 <david.torrado95@gmail.com>
2023-07-25 11:24:30 +0200

    Img

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <Container fluid className='contenedorFooter' xs={12} md={12} xl={12}>
         <div className='contenedorRRSS'>
             <a href="" target='_blank'><img src="footer/common/src/public/img/RRSS/facebook.png" alt="facebook"/></a>
-            <a href="" target='_blank'><img src="../../../public/img/RRSS/instagram.png" alt="instagram"/></a>
+            <a href="" target='_blank'><img src="src/public/img/RRSS/instagram.png" alt="instagram"/></a>
             <a href="" target='_blank'><img src="../../../public/img/RRSS/tiktok.png" alt="tiktok"/></a>
             <a href="" target='_blank'><img src="../../../public/img/RRSS/youtube.png" alt="youtube"/></a>
             <a href="" target='_blank'><img src="../../../public/img/RRSS/twitter.png" alt="twitter"/></a>
