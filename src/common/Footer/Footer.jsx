53b210e023e40b901e8c5187ff1de62354d235a4
DavidTL95 <david.torrado95@gmail.com>
2023-07-25 10:16:05 +0200

    deploy images

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -9,11 +9,11 @@ export const Footer = () => {
   return (
     <Container fluid className='contenedorFooter' xs={12} md={12} xl={12}>
         <div className='contenedorRRSS'>
-            <a href="" target='_blank'><img src="../../../img/RRSS/facebook.png" alt="facebook"/></a>
-            <a href="" target='_blank'><img src="../../../img/RRSS/instagram.png" alt="instagram"/></a>
-            <a href="" target='_blank'><img src="../../../img/RRSS/tiktok.png" alt="tiktok"/></a>
-            <a href="" target='_blank'><img src="../../../img/RRSS/youtube.png" alt="youtube"/></a>
-            <a href="" target='_blank'><img src="../../../img/RRSS/twitter.png" alt="twitter"/></a>
+            <a href="" target='_blank'><img src="../../../public/img/RRSS/facebook.png" alt="facebook"/></a>
+            <a href="" target='_blank'><img src="../../../public/img/RRSS/instagram.png" alt="instagram"/></a>
+            <a href="" target='_blank'><img src="../../../public/img/RRSS/tiktok.png" alt="tiktok"/></a>
+            <a href="" target='_blank'><img src="../../../public/img/RRSS/youtube.png" alt="youtube"/></a>
+            <a href="" target='_blank'><img src="../../../public/img/RRSS/twitter.png" alt="twitter"/></a>
         </div>
     </Container>
   )
